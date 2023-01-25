c2a30060ca1ebd1c64ca5ea901c7a1c08855bff5
Karthik Palusa <karthik.palusa@gmail.com>
2023-01-24 21:02:22 -0800

    main chapter 2 content

---
+++ ChTwo/add.h
@@ -0,0 +1,7 @@
+#ifndef ADD_H
+#define ADD_H
+// 2) This is the content of the .h file, which is where the declarations go
+int add(int x, int y); // function prototype for add.h -- don't forget the semicolon!
+
+
+#endif
---
+++ ChTwo/main.cpp
@@ -0,0 +1,10 @@
+#include "add.h" // Insert contents of add.h at this point.  Note use of double quotes here.
+#include <iostream>
+
+int add(int x, int y); // needed so main.cpp knows that add() is a function defined elsewhere
+
+int main()
+{
+    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n'; // compile error
+    return 0;
+}
